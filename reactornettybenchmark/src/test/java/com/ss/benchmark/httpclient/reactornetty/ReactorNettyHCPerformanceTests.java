a218fc31373c3c319a010f54c8e77cbc9407e3b8
Rick Hogge <rhogge@paypal.com>
2019-01-07 14:33:08 -0600

    cleanup unused imports

--- reactornettybenchmark/src/test/java/com/ss/benchmark/httpclient/reactornetty/ReactorNettyHCPerformanceTests.java
+++ reactornettybenchmark/src/test/java/com/ss/benchmark/httpclient/reactornetty/ReactorNettyHCPerformanceTests.java
@@ -2,8 +2,6 @@ package com.ss.benchmark.httpclient.reactornetty;
 
 import com.ss.benchmark.httpclient.HC;
 import com.ss.benchmark.httpclient.HCPerformanceTests;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 /**
@@ -11,8 +9,6 @@ import org.testng.annotations.Test;
  */
 @Test(groups = "performance")
 public class ReactorNettyHCPerformanceTests extends HCPerformanceTests {
-
-    private static final Logger logger = LoggerFactory.getLogger(ReactorNettyHCPerformanceTests.class);
 
     @Override
     protected HC getClient() {
